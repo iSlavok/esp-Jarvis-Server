92126a16f92ad385f094598fab1db5cd31ae3a24
iSlavok <slavaterehin6@gmail.com>
2025-02-13 07:56:01 +0300

    edit upload file argument name

--- gemini.py
+++ gemini.py
@@ -26,7 +26,7 @@ class Gemini:
 
     def generate_from_voice(self, file_num: int) -> str:
         t = time.time()
-        file = self.client.files.upload(path=f'audio/{file_num}.wav')
+        file = self.client.files.upload(file=f'audio/{file_num}.wav')
         result = self.chat.send_message(
             [
                 ".",
